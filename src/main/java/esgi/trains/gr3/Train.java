f08348c6dbe5a21cbdb364cfde1258b01e79b09f
Fabio <fabio.dugay@gmail.com>
2025-12-12 16:11:35 +0100

    correction Train

--- src/main/java/esgi/trains/gr3/Train.java
+++ src/main/java/esgi/trains/gr3/Train.java
@@ -1,12 +1,13 @@
 package esgi.trains.gr3;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Train {
     private List<Car> cars;
 
     public Train () {
-        this.cars = new List<Car>();
+        this.cars = new ArrayList<Car>();
     }
 
     public Boolean attach(Car car){
